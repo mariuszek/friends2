6a56237b6455573969cd172bd153dcdad1a152cf
Mariusz_Rog <mariusz.rog@gmail.com>
2025-07-14 23:53:57 +0200

    2 stuff

--- app.py
+++ app.py
@@ -6,16 +6,24 @@ st.set_page_config(page_title="World of Asia R.")
 # Define the photos and titles
 photo_data = {
     "2025": [
-        {"image": "Scan07.04.2025182414_001.jpg", "title": "Design 1"},
-        {"image": "Scan07.04.2025182554_001.jpg", "title": "Design 1"},
-        {"image": "Scan07.04.2025182647_001.jpg", "title": "Design 1"},
-        {"image": "Scan07.04.2025182746_001.jpg", "title": "Design 1"}
+        {"image": "2025/P_20250326_195228.jpeg", "title": "World in our hands"}
+    ],
+    "2015": [
+        {"image": "2015/P04_jpg.jpg", "title": "First..."}
+    ],
+    "Stories": [
+        {"image": "Scan07.04.2025182414_001.jpg", "title": "Part 1"},
+        {"image": "Scan07.04.2025182554_001.jpg", "title": "Part 2"},
+        {"image": "Scan07.04.2025182647_001.jpg", "title": "Part 3"},
+        {"image": "Scan07.04.2025182746_001.jpg", "title": "Part 4"}
 
-       ]
+    ]
+
+
 }
         
 # Create tabs
-tabs = st.tabs([ "2024", "2025","2026","2027"])
+tabs = st.tabs([ "2025","2015", "Stories"])
 
 # Display photos for each tab
 for tab_name, tab in zip(photo_data.keys(), tabs):
@@ -25,6 +33,7 @@ for tab_name, tab in zip(photo_data.keys(), tabs):
         cols = st.columns(1)
         for idx, photo in enumerate(photos):
             with cols[idx % 1]:
-                st.image(photo["image"], width=1200)
+
+                st.image(photo["image"], width=1200,  )
                 st.write(photo["title"])
                 st.markdown("---")
