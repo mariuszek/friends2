4efaf55c2760cd1b427227991f6b07e8b40f2819
Mariusz Róg <mariuszek@users.noreply.github.com>
2025-07-19 17:20:54 +0000

    after review Asia

--- gallery.py
+++ gallery.py
@@ -1,6 +1,5 @@
 photo_data = {
     "markers": [
-        {"image": "markers/P_20250719_173809.jpg", "title": ""},
         {"image": "markers/P_20250719_173820.jpg", "title": ""},
         {"image": "markers/P_20250719_173828.jpg", "title": ""},
         {"image": "markers/P_20250719_173857.jpg", "title": ""}
@@ -9,13 +8,11 @@ photo_data = {
         {"image": "pastels/P_20250719_173842.jpg", "title": ""}
     ],
     "paints": [
+        {"image": "paints/P_20250326_195228.jpeg", "title": ""},
         {"image": "paints/P_20250719_173756.jpg", "title": ""}
     ],
     "comics": [
-        {"image": "comics/Scan07.04.2025182414_001.jpg", "title": "Part 1"},
-        {"image": "comics/Scan07.04.2025182554_001.jpg", "title": "Part 2"},
-        {"image": "comics/Scan07.04.2025182647_001.jpg", "title": "Part 3"},
-        {"image": "comics/Scan07.04.2025182746_001.jpg", "title": "Part 4"}
+         {"image": "comics/P_20250719_173809.jpg", "title": ""}
     ]
 }
 photo_tabs = ["markers", "pastels", "paints", "comics"]
